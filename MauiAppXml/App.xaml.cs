511efbed51d684289efdd271a0b5153fff340f41
RafaelFavoretoTheos <134397718+RafaelFavoretoTheos@users.noreply.github.com>
2023-10-13 20:21:34 -0300

    Add project files.

---
+++ MauiAppXml/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppXml
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
